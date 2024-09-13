90aa416257c680edde4ee67c47af57c38a763c4a
Nathan Huang <longhaoh1370@gmail.com>
2024-09-13 15:16:54 -0400

    implement divide operation

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,7 @@ const calc = new Calculator();
 
 const result_add = calc.add(2, 3);
 const result_subtract = calc.subtract(4, 3);
+const result_divide = calc.divide(6, 2);
 console.log(`result_add = ${result_add}`);
 console.log(`result_subtract = ${result_subtract}`);
+console.log(`result_divide = ${result_divide}`);
